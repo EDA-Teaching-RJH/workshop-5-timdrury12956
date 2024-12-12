876354b3d9a69a1c4d4d0aae9a7645552fddc593
timdrury12956 <twd4@kent.ac.uk>
2024-12-12 15:51:25 +0000

    Update homework2.c

--- homework2.c
+++ homework2.c
@@ -1,1 +1,37 @@
-// homework2
+//Homework 2 - twd4
+
+//Demonstrate use of:
+//Pointers
+//Dynamic Memory Allocation
+//Process Management (forking, semaphores, priority, pipes)
+
+//Include necessary libraries
+#inlcude <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <fcntl.h>
+#inlcude <sys/stat.h>
+
+//Creating child processes
+int main() {
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    //Child process
+    printf("Child process: PID = %d, Parent PID = %d\n", getpid(), getppid());
+} else if (pid > 0) {
+    //Parent process
+    printf("Parent process: PID = %d, Child PID = %d, Child PID = %d\n", getpid, pid);
+} else {
+    //Fork failed
+    print(stderr, "Fork failed\n");
+    return 1;
+}
+return 0;
+}
